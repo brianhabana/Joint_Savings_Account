4c29edd4f2d6502c0d073f0fe97ff302dcaa4891
brianhabana <brianhabana@gmail.com>
2021-08-16 14:36:53 -0700

    updated

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,12 +1,18 @@
 /*
 Joint Savings Account
 ---------------------
+
 To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
+
 The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
 You will do the following:
+
 1. Create and work within a local blockchain development environment using the JavaScript VM provided by the Remix IDE.
+
 2. Script and deploy a **JointSavings** smart contract.
+
 3. Interact with your deployed smart contract to transfer and withdraw funds.
+
 */
 
 pragma solidity ^0.5.0;
@@ -23,7 +29,7 @@ contract JointSavings {
     address payable accountOne;
     address payable accountTwo;
     address public lastToWithdraw;
-    uint public lastWithdrawAmount;
+    uint public lastWidthdrawAmount;
     uint public contractBalance;
 
     /*
@@ -36,25 +42,26 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
-        require(recipient == accountOne || recipient == accountTwo, 'You dont own this account!');
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
 
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
-        require (contractBalance >= amount, 'Insufficient funds!' );
+        require(contractBalance <= amount, "Insufficient funds!");
 
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
         */
-       if (lastToWithdraw != recipient) {
-           lastToWithdraw = recipient ;
-       }
+        if (lastToWithdraw != recipient) {
+            lastToWithdraw = recipient;
+        }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
         recipient.transfer(amount);
+ 
 
         // Set  `lastWithdrawAmount` equal to `amount`
-        lastWithdrawAmount = amount;
+        lastWidthdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
         contractBalance = address(this).balance;
@@ -83,3 +90,4 @@ contract JointSavings {
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
     function() external payable {}
+}
